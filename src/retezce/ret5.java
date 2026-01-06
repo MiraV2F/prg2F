d11e5929b3c5d276030f989dc274ea248336b1a5
miroslav.vavrina <miroslav.vavrina@infis.cz>
2026-01-06 09:59:46 +0100

    3

---
+++ src/retezce/ret5.java
@@ -0,0 +1,13 @@
+package retezce;
+
+public class ret5 {
+    public static void main(String[] args) {
+        String rc = "885445/5665";
+        if (rc.charAt(6) == '/') {
+            System.out.println("správně");
+        }
+        else {
+            System.out.println("Nesprávně");
+        }
+    }
+}
