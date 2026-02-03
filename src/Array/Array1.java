01041231cc524cf7905939281aed0427ac0cd6f8
miroslav.vavrina <miroslav.vavrina@infis.cz>
2026-02-03 10:07:19 +0100

    3

--- src/Array/Array1.java
+++ src/Array/Array1.java
@@ -1,9 +1,36 @@
 package Array;
-
-import java.util.Arrays;
+import java.util.Scanner;
 public class Array1 {
     public static void main(String[] args) {
-        String[] znacka = "D54GD982";
+                String[][] znackyKraju = {
+                        {"Hlavní město Praha","A"},
+                        {"Středočeský kraj","S"},
+                        {"Ústecký kraj","U"},
+                        {"Liberecký kraj","L"},
+                        {"Karlovarský kraj","K"},
+                        {"Královehradexký kraj","H"},
+                        {"Pardubický kraj","E"},
+                        {"Plzeňský kraj","P"},
+                        {"Jihočeský kraj","C"},
+                        {"Kraj Vysočina","J"},
+                        {"Jihomoravský kraj","B"},
+                        {"Olomoucký kraj","M"},
+                        {"Moravskoslezský kraj","T"},
+                        {"Zlínský kraj","Z"}};
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Zadejte písmeno kraje z Vaší SPZ: ");
+                String znacka = scanner.nextLine();
+                for (int i = 0; i < znackyKraju.length; i++) {
+                    for (int j = 0; j < znackyKraju[i].length; j++) {
+                        if (znackyKraju[i][j].equalsIgnoreCase(znacka)) {
+                            System.out.println(znackyKraju[i][j-1]);
+                        }
+                    }
+                }
 
-    }
-}
+            }
+
+
+        }
+
+
---
+++ src/tabulak/pr.java
@@ -0,0 +1,11 @@
+package tabulak;
+
+public class pr {
+    public static void main(String[] args) {
+        int[][] tabulka = {{1,2,3}, {4,5,6}};
+        System.out.println("řádků: " + tabulka.length);
+        System.out.println("sloupců: " + tabulka.length);
+
+
+    }
+}
