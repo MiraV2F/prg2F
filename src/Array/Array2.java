bbc5dc5c3298f92364762dfd51562469d3eb6863
miroslav.vavrina <miroslav.vavrina@infis.cz>
2026-02-03 10:35:14 +0100

    3

--- src/Array/Array2.java
+++ src/Array/Array2.java
@@ -2,6 +2,20 @@ package Array;
 
 public class Array2 {
     public static void main(String[] args) {
+        String[][] svátky = {
+                2. ledna: Karina, Vasil.
+        3. ledna: Radmila, Radomil.
+        4. ledna: Diana, Dajana.
+        6. ledna: Kašpar, Melichar, Baltazar (Tři králové).
+        8. ledna: Čestmír.
+        9. ledna: Vladan, Valtr.
+        14. ledna: Radovan.
+        15. ledna: Alice.
+        20. ledna: Ilona, Sebastián.
+        23. ledna: Zdeněk.
+        24. ledna: Milena, Amanda.
+        30. ledna: Robin, Erna.
+        31. ledna: Marika, Marieta};
 
     }
 }
