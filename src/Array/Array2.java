e3a220f342198a9b9758112d93849b7d9259d923
MiraV2F <miroslav.vavrina@infis.cz>
2026-02-10 09:34:25 +0100

    5

--- src/Array/Array2.java
+++ src/Array/Array2.java
@@ -1,21 +1,30 @@
 package Array;
-
+import java.util.Scanner;
 public class Array2 {
     public static void main(String[] args) {
-        String[][] svátky = {
-                2. ledna: Karina, Vasil.
-        3. ledna: Radmila, Radomil.
-        4. ledna: Diana, Dajana.
-        6. ledna: Kašpar, Melichar, Baltazar (Tři králové).
-        8. ledna: Čestmír.
-        9. ledna: Vladan, Valtr.
-        14. ledna: Radovan.
-        15. ledna: Alice.
-        20. ledna: Ilona, Sebastián.
-        23. ledna: Zdeněk.
-        24. ledna: Milena, Amanda.
-        30. ledna: Robin, Erna.
-        31. ledna: Marika, Marieta};
-
-    }
+        String[][] svatky = {
+                {"2.1: Karina, Vasil."},
+                {"3.1: Radmila, Radomil."},
+                {"4.1: Diana, Dajana."},
+                {"6.1: Kašpar, Melichar, Baltazar (Tři králové)."},
+                {"8.1: Čestmír."},
+                {"9.1: Vladan, Valtr."},
+                {"14.1: Radovan."},
+                {"15.1: Alice"},
+                {"20.1: Ilona, Sebastián."},
+                {"23.1: Zdeněk."},
+                {"24.1: Milena, Amanda."},
+                {"30.1: Robin, Erna."},
+                {"31.1: Marika, Marieta"}};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Zadejte datum svátku: ");
+        String svatek = scanner.nextLine();
+        for (int i = 0; i < svatky.length; i++) {
+            for (int j = 0; j < svatky[i].length; i++) {
+                if (svatky[i][j].equalsIgnoreCase(svatek)) {
+                    System.out.println(svatky[i][j-1]);
+                }
+        }
+}
+}
 }
