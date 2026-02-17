6d3d3d6bb6d06d3519befd7c3ec147d072094062
miroslav.vavrina <miroslav.vavrina@infis.cz>
2026-02-17 09:55:29 +0100

    3

---
+++ src/Array/Array3.java
@@ -0,0 +1,12 @@
+package Array;
+
+public class Array3 {
+    public static void main(String[] args) {
+        String[][] barvy = {
+                {"česky", "anglicky", "německy",  "španělsky"},
+                {"bílá",   "white",    "weiss",    "blanco"},
+                {"černá",  "black",    "schwarz",  "negro"}
+        };
+
+    }
+}
---
+++ src/Array/Array7.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class Array7 {
+    public static void main(String[] args) {
+        byte[][] kino = {
+            {0,0,1,1,0},
+            {0,1,1,0,0},
+            {0,0,0,0,1}
+        };
+    }
+}
